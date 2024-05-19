c46e7ce8e68d5582c1274eceff649d32947584d4
andbilous <andbilous@gmail.com>
2024-05-19 16:20:58 +0200

    initial2

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -3,6 +3,7 @@ const launchesDatabase = require("./launches.mongo");
 const planets = require("./planets.mongo");
 
 const DEFAULT_FLIGHT_NUMBER = 100;
+console.log(2424);
 
 const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
 
